3542be7f693d97cf4afd4f8f60762937086281d7
Ghifair <102426727+Ghifair@users.noreply.github.com>
2024-06-01 16:01:21 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DBController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,9 @@ Route::post('addsave', [DBController::class,'addsave']);
 Route::get('edit/{nama}', [DBController::class,'edit']);
 Route::post('editsave', [DBController::class,'editsave']);
 Route::get('delete/{nama}', [DBController::class,'delete']);
+
+Route::get('login', [LoginController::class, 'login'])->name('login');
+Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
+
+Route::get('home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
---
+++ storage/framework/views/45f902a3fb64827539833f3babd2b7bc.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/main.js" defer></script>
+</head>
+
+<body>
+    <header class="header" data-header>
+        <div class="container">
+            <img src="images/Logo1.png" alt="" class="logo">
+        </div>
+        <img src="/images/bg-pattern2.png" width="1920" height="840" role="presentation" class="bg-pattern">
+    </header>
+    <section class="main">
+        <div class="container">
+            <div class="box">
+                <h1 class="large-font">Beranda</h1>
+                <a href="/index" class="button">Daftar</a>
+                <a href="/add" class="button">Tambah</a>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
+<?php /**PATH C:\Users\muham\Documents\Kuliah\sem 6\pemweb\project\Project_Akhir-main\resources\views/home.blade.php ENDPATH**/ ?>
